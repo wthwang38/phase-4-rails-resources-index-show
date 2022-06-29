06d86243c3bc7b76c2a8ffbedd3577cf8064bb5c
William Hwang <willhwang90@gmail.com>
2022-06-29 14:12:43 -0400

    commit tree fc410c333e415ba776dacd12c6a4cb1a1b9bad32

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :birds, only: [:index, :show]
+  # get '/birds', to: 'birds#index'
+
+  # get '/birds/:id', to: 'birds#show'
 end
+ 
